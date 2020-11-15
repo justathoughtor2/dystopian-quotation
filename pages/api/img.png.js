b6a51d157d9a40ca3e6379b70f8155a7f091e33e
Jesse Patrick Bohanan <thoughthaven@gmail.com>
2020-11-15 14:01:46 -0500

    Switch to text2png

--- pages/api/img.png.js
+++ pages/api/img.png.js
@@ -1,12 +1,17 @@
 const text2png = require('text2png');
 const fs = require('fs');
 const path = require('path');
+import { registerFont } from 'canvas';
 
 module.exports = (req, res) => {
+  registerFont(path.resolve('./public/SpecialElite-Regular.ttf'), { family: 'Special Elite' });
   let data = fs.readFileSync(path.resolve('./public/data.json'), 'utf8');
   let jsonData = JSON.parse(data);
   let element = jsonData[Math.floor(Math.random() * jsonData.length)];
   res.statusCode = 200;
   res.setHeader('Content-Type', 'image/png');
-  res.send(text2png(`"${element.quote.match(/.{1,128}(\s|$)/g).join('\n')}"\n-- ${element.source} by ${element.author}`, { color: 'gray' }));
+  res.send(text2png(`"${element.quote.match(/.{1,64}(\s|$)/g).join('\n')}"\n-- ${element.source} by ${element.author}`, {
+    color: 'gray',
+    font: '40px Special Elite'
+  }));
 };
