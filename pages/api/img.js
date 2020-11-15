711d523da996a1caf5f3dc97b41c83491209e6c2
Jesse Patrick Bohanan <thoughthaven@gmail.com>
2020-11-15 13:10:58 -0500

    Fix missing font

--- pages/api/img.js
+++ pages/api/img.js
@@ -6,7 +6,10 @@ module.exports = (req, res) => {
   let data = fs.readFileSync(path.resolve('./public/data.json'), 'utf8');
   let jsonData = JSON.parse(data);
   let element = jsonData[Math.floor(Math.random() * jsonData.length)];
-  textToImage.generate(`"${element.quote}"\n-- ${element.source} by ${element.author}`, { width: 720 }).then(function (dataUri) {
+  textToImage.generate(`"${element.quote}"\n-- ${element.source} by ${element.author}`, {
+    width: 720,
+    fontFamily: 'DejaVu Sans'
+  }).then(function (dataUri) {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     res.send({quotation: dataUri});
